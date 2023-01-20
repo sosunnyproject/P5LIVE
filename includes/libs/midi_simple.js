b1a239905b7295c4d6c9b52f53168eb0971af17b
sosunnyproject <parksosun1103@gmail.com>
2023-01-20 22:15:48 +0900

    update midi for aka LPD8

--- includes/libs/midi_simple.js
+++ includes/libs/midi_simple.js
@@ -1,3 +1,6 @@
+
+// midi setting
+
 let coolors1 = ["#006ba6", "#0496ff", "#ffbc42", "#d81159", "#8f2d56"]
 let coolors2 = ["#525b76", "#ffcad4", "#b0d0d3", "#c08497", "#f7af9d"];
 let coolors3 = ["#70d6ff", "#ff70a6", "#ff9770", "#ffd670", "#e9ff70"];
@@ -6,16 +9,19 @@ let coolors4 = ["#27187e", "#758bfd", "#aeb8fe", "#f1f2f6", "#ff8600"];
 window.colors1 = coolors1;
 window.colors2 = coolors2;
 
-const CHANNEL_KEYBOARD = 1;
-const CHANNEL_PAD = 0;
+
+// midi programs
 const MIDI_CLOCK = 248;
 const TYPE_NOTE_ON = 144;
 const TYPE_NOTE_OFF = 128;
+const CHANNEL_KEYBOARD = 1;
+const CHANNEL_PAD = 0;
+const CHANNEL_2 = 2;
+const CHANNEL_3 = 3;
 const TYPE_KNOB = 176;
-const NOTE1 = 36, NOTE2 = 37, NOTE3 = 38, NOTE4 = 39, NOTE5 = 40, NOTE6 = 41;
+const PAD1 = 36, PAD2 = 37, PAD3 = 38, PAD4 = 39, PAD5 = 40, PAD6 = 41;
 
 // 미디 연결
-// always call setupMidi() in your sketch
 function setupMidi() {
     if (navigator.requestMIDIAccess) console.log('이 브라우저는 WebMIDI를 지원합니다!')
     else console.log('WebMIDI가 실행되지 않습니다.')
@@ -38,16 +44,19 @@ function onMIDIMessage(message) {
     type = data[0] & 0xf0
     note = data[1]
     velocity = data[2]
-
+    
+    // console.log(data);
+	// console.log(channel, type, note);
     // 248 is midi clock
     if (data[0] != MIDI_CLOCK) {    
         if(type === TYPE_NOTE_ON) {
+        	console.log(channel, type, note, velocity);
             if(channel === CHANNEL_KEYBOARD) {
                 keyboardPressed(channel, note, velocity);
-
             } else if(channel === CHANNEL_PAD) { 
                 padPressed(channel, note, velocity);
-                changeColors(note);
+            } else { 
+                console.log(`etc: channel: ${channel}, note: ${note}, vel: ${velocity}`);
             }
         } else if(type === TYPE_KNOB) {
             knobChanged(channel, note, velocity);
@@ -59,33 +68,17 @@ function onMIDIMessage(message) {
 // PAD1.NOTE36  PAD2.NOTE37  PAD3.NOTE38  PAD4.NOTE39
 function padPressed() {
     console.log(`PAD: channel: ${channel}, note: ${note}, vel: ${velocity}`);
-    switch(note) {
-        case 0:
-            break;
-        case 1:
-            break;
-        case 2:
-            break;
-        case 8:
-            break;
-        case 9:
-            break;
-        case 10:
-            break;
-        case 16:
-            break;
-        case 17:
-            break;
-        case 18:
-            break;
-    }
+    changeColors(note);
 }
 function keyboardPressed() {
     console.log(`KEYBOARD: channel ${channel}, note: ${note}, vel: ${velocity}`);
 }
 function knobChanged(channel, note, velocity){
     console.log(`KNOBS: channel ${channel}, note: ${note}, vel: ${velocity}`);
-    switch(note) {
+	changeKnobValues();
+}
+function changeKnobValues() {
+	switch(note) {
         case 1:
             window.KNOB_1 = velocity;
             break;
@@ -112,24 +105,20 @@ function knobChanged(channel, note, velocity){
             break;
     }
 }
-function changeCoolor(note) {
-    if(NOTE6 >= note && note >= NOTE1) {
-        changeColors(note);
-    }
-}
+
 function changeColors(note) {
     switch(note){
-        case NOTE1:
-            window.colors1 = col1;
+        case PAD1:
+            window.colors1 = coolors1;
             break;
-        case NOTE2:
-            window.colors1 = col2;
+        case PAD2:
+            window.colors1 = coolors2;
             break;
-        case NOTE3:
-            window.colors1 = col3;
+        case PAD3:
+            window.colors1 = coolors3;
             break;
-        case NOTE4:
-            window.colors1 = col4;
+        case PAD4:
+            window.colors1 = coolors4;
             break;
     }
 }
